342adab026b75c4b917102675a3aeb5e15e69af5
emeryberg <emery.berg@gmail.com>
2015-11-26 23:19:53 -0800

    disappointing doom blade

--- core/src/verberg/com/shmup/Level.java
+++ core/src/verberg/com/shmup/Level.java
@@ -10,17 +10,20 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.physics.box2d.joints.RevoluteJointDef;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
+
+import java.util.HashMap;
 
 /**
  * Created by Orion on 11/17/2015.
  */
 public class Level {
-    //NOTE: Not sure if this was your intended use... but can be moved elsewhere if needed
-
     //move to definitions class
     final int PPM = 5;
+
+    private HashMap<String, Body> bodies;
     public World world;
 
     public void create(World world) {
@@ -34,23 +37,35 @@ public class Level {
         JsonValue json = reader.parse(level);
         JsonValue map = json.get("level");
         JsonValue blocks = map.get("blocks");
+        Body body;
 
         for(JsonValue block : blocks){
+            //String name = block.get("name").asString();
             float[] pos = block.get("location").asFloatArray();
             float[] size = block.get("size").asFloatArray();
             float friction = block.get("friction").asFloat();
             float density = block.get("density").asFloat();
             BodyDef.BodyType type = block.get("dynamic").asBoolean() ? BodyType.DynamicBody : BodyType.StaticBody;
 
-            createBox(pos[0], pos[1], size[0], size[1], friction, density, type);
+            body = createBox(pos[0], pos[1], size[0], size[1], friction, density, type);
+            //bodies.put(name, body);
         }
 
-        createCircle(160, 155, 5, 1, 1, BodyType.StaticBody);
-        createBox(165, 154.5f, 75, 2, 1, 1, BodyType.StaticBody);
-        createBox(80, 154.5f, 75, 2, 1, 1, BodyType.StaticBody);
+        Body circle = createCircle(160, 155, 5, 1, 1, BodyType.StaticBody);
+        Body blade = createBox(165, 154.5f, 150, 2, 1, 1, BodyType.DynamicBody);
+
+        RevoluteJointDef joint = new RevoluteJointDef();
+        joint.bodyA = circle;
+        joint.bodyB = blade;
+        joint.localAnchorB.set(0,0);
+        joint.enableMotor = true;
+        joint.maxMotorTorque = 100000.0f;
+        joint.motorSpeed = 2f;
+        world.createJoint(joint);
+
     }
 
-    public void createBox(float x, float y, float w, float h, float friction, float density, BodyType type) {
+    public Body createBox(float x, float y, float w, float h, float friction, float density, BodyType type) {
         //box2d doubles these when it creates the box... so I am undoing that so coords are consistant
         w /= 2;
         h /= 2;
@@ -71,9 +86,11 @@ public class Level {
 
         body.createFixture(fixture);
         box.dispose();
+
+        return body;
     }
 
-    public void createCircle(float x, float y, float r, float friction, float density, BodyType type) {
+    public Body createCircle(float x, float y, float r, float friction, float density, BodyType type) {
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = type;
         bodyDef.position.set(new Vector2(x, y));
@@ -90,5 +107,7 @@ public class Level {
 
         body.createFixture(fixture);
         circle.dispose();
+
+        return body;
     }
 }
